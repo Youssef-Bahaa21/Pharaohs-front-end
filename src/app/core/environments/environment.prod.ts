60f8c675eaea929656323da54dbd1734afdd3a58
Youssef Bahaa <youssefbahaa211@gmail.com>
2025-06-19 01:04:55 +0300

    ff

---
+++ src/app/core/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://pharaoh-s-backend.railway.app/api',
+    baseUrl: 'https://pharaoh-s-backend.railway.app'
+}; 
--- src/app/core/environments/environments.ts
+++ src/app/core/environments/environments.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     apiUrl: window.location.hostname === 'localhost' ?
         'http://localhost:3000/api' :
-        `http://${window.location.hostname}:3000/api`,
+        'https://pharaoh-s-backend.railway.app/api',
     baseUrl: window.location.hostname === 'localhost' ?
         'http://localhost:3000' :
-        `http://${window.location.hostname}:3000`
+        'https://pharaoh-s-backend.railway.app'
 };
