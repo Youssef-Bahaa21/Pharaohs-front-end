af2ed2c25575131630ddec8a7f595046a3300560
Your Name <youremail@example.com>
2025-06-19 15:38:10 +0300

    EV

--- src/app/core/environments/environments.ts
+++ src/app/core/environments/environments.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiUrl: window.location.hostname === 'localhost' ?
         'http://localhost:3000/api' :
-        'https://pharaohs-be-production.up.railway.app',
+        'https://pharaohs-be-production.up.railway.app/api',
     baseUrl: window.location.hostname === 'localhost' ?
         'http://localhost:3000' :
         'https://pharaohs-be-production.up.railway.app'
