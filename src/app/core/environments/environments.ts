c64dbe081a61b4e2775bbffb4045116a79a7bbd4
Youssef Bahaa <youssefbahaa211@gmail.com>
2025-06-19 14:42:58 +0300

    ss

--- src/app/core/environments/environments.ts
+++ src/app/core/environments/environments.ts
@@ -1,16 +1,9 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',
-    cloudinary: {
-        cloudName: 'dk0szadna',
-        uploadPreset: 'pharaohs_uploads'
-    },
-    videoConfig: {
-        maxSize: 100 * 1024 * 1024, // 100MB
-        allowedTypes: ['video/mp4', 'video/webm', 'video/quicktime']
-    },
-    imageConfig: {
-        maxSize: 5 * 1024 * 1024, // 5MB
-        allowedTypes: ['image/jpeg', 'image/png', 'image/jpg', 'image/webp']
-    }
+    apiUrl: window.location.hostname === 'localhost' ?
+        'http://localhost:3000/api' :
+        `http://${window.location.hostname}:3000/api`,
+    baseUrl: window.location.hostname === 'localhost' ?
+        'http://localhost:3000' :
+        `http://${window.location.hostname}:3000`
 };
